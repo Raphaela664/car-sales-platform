d8c2242585ca9b3fd47cfc93c47299a7ff1a2004
Raphaela664 <mahoraphy02@gmail.com>
2023-07-20 12:38:26 +0200

    add search functionalities

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,30 @@
 "use client"
 import { useState } from 'react';
-
+import Image from 'next/image';
 import {SearchManufacturer} from "./"
+
+const SearchButton =({otherClasses} : {otherClasses : string }) =>(
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image src="/ magnifying-glass.svg" 
+      alt ="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+      />
+  </button>
+)
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('')
   const handleSearch = ()=>{}
   return (
-    <form className='searchBar' onSubmit={handleSearch}>
-        <div className="searchBar__item">
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className="searchbar__item">
             <SearchManufacturer 
              manufacturer={manufacturer}
              setManufacturer={setManufacturer}
             />
+            <SearchButton otherClasses="sm:hidden" />
         </div>
     </form>
   )
